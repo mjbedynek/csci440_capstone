8689293457e6498cb7f928b68b254ede39e9184c
mjbedynek <32271070+mjbedynek@users.noreply.github.com>
2019-11-15 23:15:58 -0500

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,57 +1,42 @@
 <html>
-<title>Inserted Post</title>
+  <title>Inserted Post</title>
 <body>
 
-<?php
+  <?php
 
-// get the data from the form and assign the data to variables
-$title = $_POST['Title'];
-$body = $_POST['Body'];
-$date = $_POST['post_date'];
-$author = $_POST['author'];
+  include "connect_query.php";
 
-// check to see if all the data is there
-if (!$title || !$body || !$date || !$author)
-{
-	echo "You have not entered all the required details.<br>"
+  // get the data from the form and assign the data to variables
+  $title = $_POST['Title'];
+  $body = $_POST['Body'];
+
+  // check to see if all the data is there
+  if (!$title || !$body)
+  {
+    echo "You have not entered all the required details.<br>"
 		."Please go back and try again.";
-    echo '<p><a href = "https://william-young.000webhostapp.com/insert_form.html">Go Back</a></p>';
-	exit;
-}
-
-// add slashes and prepare the data for inserting into the db
-$title = addslashes($title);
-$body = addslashes($body);
-$date = addslashes($date);
-$author = addslashes($author);
-
-// connect to the db
-$conn = new mysqli("localhost","id7820654_willyoung18","/*Password*/","id7820654_blog_posts");
-if (!$conn) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . $conn->connect_errno() . PHP_EOL;
-    echo "Debugging error: " . $conn->connect_error() . PHP_EOL;
+    echo '<p><a href = "http://no-carrier.org/insert_form.php">Go Back</a></p>';
     exit;
-}
+  }
 
-// prepare the query
-$query = "insert into posts values
-	('".NULL."','".$title."','".$body."','".$date."','".NULL."','".$author."')";
+  // add slashes and prepare the data for inserting into the db
+  $title = addslashes($title);
+  $body = addslashes($body);
 
-// run the query
-$result = $conn->query($query);
-/*
-if ($result) {
-    // output data of each row
-        echo "id: " . $row["postsid"]. " - Name: " . $row["Title"]. " " . $row["Body"]. "<br>";
-    
-} else {
-    echo "0 results";
-}
-*/
+  // INSERT INTO posts (authorid, body, title) VALUES (1, "testing a msg", "routine test")
+  
+  // set to admin for now - change later
+  $authorid = 1;
 
+  // prepare the query
+  $sql = "insert into posts (authorid, title, body) VALUES (1,"
+            ."'".$title."','".$body."')";
+  // Connect to the database and run query.
+  connect_query($sql);
 
-?>
-<script type = "text/javascript" > location.href = 'https://william-young.000webhostapp.com/blog_posts.php'; </script>
+  ?>
+
+  <script type = "text/javascript" > location.href = 'http://no-carrier.org/all_posts.php'; </script>
+
 </body>
 </html>
