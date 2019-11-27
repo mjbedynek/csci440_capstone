1ca2801ac3c3087b1e242bc885ecb94a33d282a5
mjbedynek <32271070+mjbedynek@users.noreply.github.com>
2019-11-26 23:56:26 -0500

    Start building blog class

---
+++ include/Blog.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once "Database.php";
+
+class Blog {
+	// Instance variables
+	private $dbh;
+
+	// Constructor
+	function __construct() {
+      // Open a connection to the DB
+		$this->dbh = new Database();
+   }
+   function getMostRecentPost() {
+      // No need to parameterize... no user input sent to DB
+      $sql = 'SELECT title, body, postdatetime, displayname FROM posts
+              LEFT JOIN users ON posts.authorid = users.id
+              ORDER BY postdatetime DESC LIMIT 1';
+      $res = $this->dbh->query($sql, null)[0];
+      return $res;
+   }
+}
+
+?>
