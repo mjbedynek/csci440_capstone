77e9e60c974edda05fc9b0578674108af03fdc0b
mjbedynek <32271070+mjbedynek@users.noreply.github.com>
2019-11-26 23:59:02 -0500

    use tabs not spaces

--- login.php
+++ login.php
@@ -1,16 +1,16 @@
 <?php
 
-require_once "include/DB.php";
+require_once "include/Database.php";
 require_once "include/User.php";
 
 $html_menu = file_get_contents("menu.php");
 $html = '<head>
-	<link rel="stylesheet" href="styles.css">
-	<link rel="stylesheet" href="restyle.css">
+   <link rel="stylesheet" href="styles.css">
+   <link rel="stylesheet" href="restyle.css">
 </head>
 
 <body>
-	<header><img src = "tamuc-logo.png" alt = "TAMUC" />
+   <header><img src = "tamuc-logo.png" alt = "TAMUC" />
 
 <div class = "flexbox">'.$html_menu.
 '</div>
@@ -23,27 +23,15 @@ $html = '<head>
 session_start();
 
 if ( isset( $_POST['uname'] ) && isset( $_POST['psw'] ) ) {
-	$db = new Database();
- 	$user = new User($db, $_POST['uname'], $_POST['psw']);
-	if ($user->isAuthenticated()) {
+   $user = new User($_POST['uname'], $_POST['psw']);
+   if ($user->isAuthenticated()) {
          $_SESSION['id'] = $user->getID();
-   		$_SESSION['username'] = $_POST['uname'];
-		if ($user->isAdmin())
-			$_SESSION['isadmin'] = true;
-		$html .= "<script type = 'text/javascript' > location.href = 'admin_home.php'; </script>";
-	} else {
-		$html .= "<h1>Invalid Username or Password. Please try again or return to the <a href = '/'>Home Page</a></h1>";
-
-    	//if ( password_verify( $_POST['password'], $user->password ) ) {
-        // Getting submitted user data from database
-        //$con = new mysqli($db_host, $db_user, $db_pass, $db_name);
-        //$stmt = $con->prepare("SELECT * FROM users WHERE username = ?");
-        //$stmt->bind_param('s', $_POST['username']);
-        //$stmt->execute();
-        //$result = $stmt->get_result();
-    	//$user = $result->fetch_object();
-
-    	// Verify user password and set $_SESSION
+         $_SESSION['username'] = $_POST['uname'];
+      if ($user->isAdmin())
+         $_SESSION['isadmin'] = true;
+      $html .= "<script type = 'text/javascript' > location.href = 'admin_home.php'; </script>";
+   } else {
+      $html .= "<h1>Invalid Username or Password. Please try again or return to the <a href = '/'>Home Page</a></h1>";
     }
 }
 
