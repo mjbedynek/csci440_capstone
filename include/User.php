6e98fd2aeacd20206ac6e21afc23dd761147de90
mjbedynek <32271070+mjbedynek@users.noreply.github.com>
2019-11-26 23:55:47 -0500

    update
    makes its own db handle rather than being passed as reference

--- include/User.php
+++ include/User.php
@@ -1,61 +1,63 @@
 <?php
 
-require_once "DB.php";
+require_once "Database.php";
 
 class User {
 
-	// Instance variables
-	private $dbh;
-	private $authenticated;
-	private $id;
-	private $username;
-	private $lastlogin;
-	private $isadmin;
+   // Instance variables
+   private $dbh;
+   private $authenticated;
+   private $id;
+   private $username;
+   private $lastlogin;
+   private $isadmin;
 
-	// Constructor
-	function __construct(&$pdo, $username, $password) {
-		$this->dbh = $pdo;
-		// See if user exists
-		$sql = 'SELECT id, username, isadmin FROM users WHERE username = "' . $username.
-				'" AND password = "'.$password.'"';
-		$res = $this->dbh->query($sql)[0];
+   // Constructor
+   function __construct($username, $password) {
+      // Open a connection to the DB
+      $this->dbh = new Database();
+      // Parameterize query - prevent SQL injection
+      // Also use strip tags to remove html from username and password
+      $params = [ 'username'  => strip_tags($username),
+                  'password'  => strip_tags($password) , ];
+      $sql = 'SELECT id, username, isadmin FROM users WHERE '.
+             'username = :username AND password = :password';
+      $res = $this->dbh->query($sql, $params)[0];
 
-		if ($res) {
-			//Remove after testing complete
-			//var_dump($res);
+      // If we get any results then the username is valid
+      if ($res) {
+         // Store instance vars for obj
+         $this->id = $res['id'];
+         $this->username = $res['username'];
+         $this->lastlogin = $res['lastlogin'];
+         // Recast string value to boolean
+         $this->isadmin = filter_var($res['isadmin'], FILTER_VALIDATE_BOOLEAN);
+         $this->authenticated = true;
+      }
+   }
 
-			// Store instance vars for obj
-			$this->id = $res['id'];
-			$this->username = $res['username'];
-			$this->lastlogin = $res['lastlogin'];
-			// Recast string value to boolean
-			$this->isadmin = filter_var($res['isadmin'], FILTER_VALIDATE_BOOLEAN);
-			$this->authenticated = true;
-		}
-	}
+   // Return id (from DB)
+   public function getID() {
+      return $this->id;
+   }
 
-	// Return id (from DB)
-	public function getID() {
-		return $this->id;
-	}
+   // Return username for object
+   public function getUserName() {
+      return $this->username;
+   }
 
-	// Return username for object
-	public function getUserName() {
-		return $this->username;
-	}
+   // Return if user is admin
+   public function isAdmin() {
+      return $this->isadmin;
+   }
 
-	// Return if user is admin
-	public function isAdmin() {
-		return $this->isadmin;
-	}
+   public function isAuthenticated() {
+      return $this->authenticated;
+   }
 
-	public function isAuthenticated() {
-		return $this->authenticated;
-	}
-
-	// Change the user's password
-	//public function setPassword() {
-	//}
+   // Change the user's password
+   //public function setPassword() {
+   //}
 }
 
 ?>
