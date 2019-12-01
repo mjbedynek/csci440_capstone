29fe55a8095734a712e33de8145ed42f0261810f
mjbedynek <32271070+mjbedynek@users.noreply.github.com>
2019-12-01 03:33:04 -0500

    new methods

--- include/Blog.php
+++ include/Blog.php
@@ -11,9 +11,32 @@ class Blog {
       // Open a connection to the DB
       $this->dbh = new Database();
    }
-   function getMostRecentPost() {
+   function deletePost($postID) {
+      #echo "postid: $postID<Br>";
+      // Ensure $postID is an Integer
+      if (!is_int($postID) && $postID > 0)
+         return;
+      $params = [
+         'id'  => $postID,
+         ];
+      $sql = 'DELETE FROM posts WHERE id = :id';
+      $this->dbh->execute($sql, $params);
+   }
+   function getPost($postID) {
+      // Ensure $postID is an Integer
+      if (!is_int($postID))
+         return;
+      $params = [
+         'id'  => $postID,
+         ];
+      $sql = 'SELECT posts.id, authorid, title, body, postdatetime, displayname FROM posts
+              LEFT JOIN users ON posts.authorid = users.id WHERE posts.id = :id';
+      $res = $this->dbh->query($sql, $params)[0];
+      return $res;
+   }
+   function getLatestPost() {
       // No need to parameterize... no user input sent to DB
-      $sql = 'SELECT title, body, postdatetime, displayname FROM posts
+      $sql = 'SELECT posts.id, authorid, title, body, postdatetime, displayname FROM posts
               LEFT JOIN users ON posts.authorid = users.id
               ORDER BY postdatetime DESC LIMIT 1';
       $res = $this->dbh->query($sql, null)[0];
@@ -21,12 +44,19 @@ class Blog {
    }
 
    function getPage($page, $results_per_page) {
+      // Ensure $page and $results_per_page are Integers
+      if (!is_int($page) && !is_int($results_per_page))
+         return;
       // No need to parameterize... no user input sent to DB
       $start_from = ($page - 1) * $results_per_page;
-      $sql = 'SELECT title, body, postdatetime, displayname FROM posts
+      $params = [
+         'start_from'  => $start_from,
+         'results_per_page'   => $results_per_page,
+         ];
+      $sql = 'SELECT posts.id, authorid, title, body, postdatetime, displayname FROM posts
               LEFT JOIN users ON posts.authorid = users.id
-              ORDER BY postdatetime DESC LIMIT '.$start_from.','.$results_per_page;
-      $res = $this->dbh->query($sql, null);
+              ORDER BY postdatetime DESC LIMIT :start_from,:results_per_page';
+      $res = $this->dbh->query($sql, $params);
       return $res;
    }
    function newPost(&$authorid, &$title, &$body) {
@@ -37,7 +67,7 @@ class Blog {
          'body'      => $body,
          ];
       $sql = "INSERT INTO posts (authorid, title, body) VALUES (:authorid, :title, :body)";
-      $this->dbh->insert($sql, $params);
+      $this->dbh->execute($sql, $params);
    }
 }
 
