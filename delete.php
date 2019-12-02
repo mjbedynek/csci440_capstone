125cef934b45b4a4eb9efa7ad8c1101230a51a97
mjbedynek <32271070+mjbedynek@users.noreply.github.com>
2019-12-02 00:05:16 -0500

    schema change

--- delete.php
+++ delete.php
@@ -9,15 +9,15 @@ require_once "include/html_includes.php";
 <html>
 <?
    // _GET returns strings... must cast as Integer for getPost method
-   $id = isset($_GET[ "id" ]) ? (int)$_GET[ "id" ] : 0;
+   $pid = isset($_GET[ "pid" ]) ? (int)$_GET[ "pid" ] : 0;
 
    if ( isset ( $_SESSION['username'] ) ) {
       $blog = new Blog();
       $blogPost = $blog->getPost($id);
 
       // Permit the author or admin the ability to delete the post
-      if ($_SESSION["id"] == $blogPost["authorid"] || $_SESSION["isadmin"])
-         $blog->deletePost($id);
+      if ($_SESSION["uid"] == $blogPost["authorid"] || $_SESSION["isadmin"])
+         $blog->deletePost($pid);
       else
          $html = "<p>Access denied</p>";
    } else
