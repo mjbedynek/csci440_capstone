d296c3770b9a0f2ba71a8402d358cc0cc8970b3c
mjbedynek <32271070+mjbedynek@users.noreply.github.com>
2019-12-04 01:08:25 -0500

    add method getPostCount and search

--- include/Blog.php
+++ include/Blog.php
@@ -22,18 +22,6 @@ class Blog {
       $sql = 'DELETE FROM posts WHERE pid = :pid';
       $this->dbh->execute($sql, $params);
    }
-   function getPost($postID) {
-      // Ensure $postID is an Integer
-      if (!is_int($postID))
-         return;
-      $params = [
-         'pid'  => $postID,
-         ];
-      $sql = 'SELECT pid, authorid, title, body, postdatetime, displayname FROM posts
-              LEFT JOIN users ON posts.authorid = users.uid WHERE posts.pid = :pid';
-      $res = $this->dbh->query($sql, $params)[0];
-      return $res;
-   }
    function getLatestPost() {
       // No need to parameterize... no user input sent to DB
       $sql = 'SELECT pid, authorid, title, body, postdatetime, displayname FROM posts
@@ -59,6 +47,39 @@ class Blog {
       $res = $this->dbh->query($sql, $params);
       return $res;
    }
+   function getPost($postID) {
+      // Ensure $postID is an Integer
+      if (!is_int($postID))
+         return;
+      $params = [
+         'pid'  => $postID,
+         ];
+      $sql = 'SELECT pid, authorid, title, body, postdatetime, displayname FROM posts
+              LEFT JOIN users ON posts.authorid = users.uid WHERE posts.pid = :pid';
+      $res = $this->dbh->query($sql, $params)[0];
+      return $res;
+   }
+   function getPostCount() {
+      $count = 0;
+      $searchTerm = "";
+      if (func_num_args() > 0)
+         $searchTerm = func_get_arg(0);
+      if ($searchTerm) {
+         $params = [
+            'search'    => '%'.$searchTerm.'%',
+            'search2'   => '%'.$searchTerm.'%',
+            ];
+         $sql = 'SELECT count(*) FROM posts
+                 WHERE TITLE LIKE :search OR BODY LIKE :search2';
+         $res = $this->dbh->query($sql, $params)[0];
+         $count = $res['count(*)'];
+      } else {
+         $sql = 'SELECT count(*) FROM posts';
+         $res = $this->dbh->query($sql, null)[0];
+         $count = $res['count(*)'];
+      }
+      return $count;
+   }
    function newPost(&$authorid, &$title, &$body) {
       // Parameterize data
       $params = [
@@ -68,6 +89,25 @@ class Blog {
          ];
       $sql = "INSERT INTO posts (authorid, title, body) VALUES (:authorid, :title, :body)";
       $this->dbh->execute($sql, $params);
+   }
+   function search($page, $results_per_page, $searchTerm) {
+      // Ensure $page and $results_per_page are Integers
+      if (!is_int($page) && !is_int($results_per_page))
+         return;
+      // No need to parameterize... no user input sent to DB
+      $start_from = ($page - 1) * $results_per_page;
+      $params = [
+         'start_from'         => $start_from,
+         'results_per_page'   => $results_per_page,
+         'search'             => '%'.$searchTerm.'%',
+         'search2'            => '%'.$searchTerm.'%',
+         ];
+      $sql = 'SELECT posts.pid, authorid, title, body, postdatetime, displayname FROM posts
+              LEFT JOIN users ON posts.authorid = users.uid
+              WHERE TITLE LIKE :search OR BODY LIKE :search2
+              ORDER BY postdatetime DESC LIMIT :start_from,:results_per_page';
+      $res = $this->dbh->query($sql, $params);
+      return $res;
    }
    function updatePost(&$pid, &$title, &$body) {
       // Parameterize data
